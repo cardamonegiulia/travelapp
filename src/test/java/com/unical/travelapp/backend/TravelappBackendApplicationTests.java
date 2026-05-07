c74a1f1d6e3aba7fe8eb41c4967b24281a7698be
giuliacardamone2 <crdgli04h51m208g@studenti.unical.it>
2026-05-07 18:12:44 +0200

    Reset totale: Nuova struttura monolite modulare

---
+++ src/test/java/com/unical/travelapp/backend/TravelappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unical.travelapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
