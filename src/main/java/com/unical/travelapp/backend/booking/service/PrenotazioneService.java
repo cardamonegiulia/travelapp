a0a300f895c0c90162c4c833f6ce00302db36de3
Alex107219 <alexgenca10@gmail.com>
2026-05-16 20:14:31 +0200

    programmazione di prenotazione quasi finita, migliroati alcuni metodi mettendo ordine, domani passo ai prossimi service e al controller

--- src/main/java/com/unical/travelapp/backend/booking/service/PrenotazioneService.java
+++ src/main/java/com/unical/travelapp/backend/booking/service/PrenotazioneService.java
@@ -5,11 +5,17 @@ import com.unical.travelapp.backend.booking.entity.Prenotazione;
 import com.unical.travelapp.backend.booking.repositories.ExtraPrenotazioneRepository;
 import com.unical.travelapp.backend.booking.repositories.PagamentoRepository;
 import com.unical.travelapp.backend.booking.repositories.PrenotazioneRepository;
+import com.unical.travelapp.backend.catalog.entity.DisponibilitaItinerario;
+import com.unical.travelapp.backend.catalog.entity.SessioneSingolaAttivita;
+import com.unical.travelapp.backend.catalog.repository.DisponibilitaItinerarioRepository;
+import com.unical.travelapp.backend.catalog.repository.SessioneSingolaAttivitaRepository;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -20,29 +26,80 @@ public class PrenotazioneService {
     private final ExtraPrenotazioneRepository extraPrenotazioneRepo;
     private final PagamentoRepository pagamentoRepo;
 
-    //private final UtenteRepository utenteRepository;
-    //private final DisponibilitaItinerarioRepository disponibilitaItinerarioRepository;
-    //private final SessioneSingolaAttivitaRepository sessioneSingolaAttivitaRepository;
 
-    // primo metodo mongolo, giusto per iniziare
-    public List<Prenotazione> getAllPrenotazioni() {
-        return prenotazioneRepo.findAll();
+    //private final UtenteRepository utenteRepository;
+    private final DisponibilitaItinerarioRepository disponibilitaItinerarioRepository;
+    private final SessioneSingolaAttivitaRepository sessioneSingolaAttivitaRepository;
+
+
+    // cretePrenotazione era diventata troppo grande quindi ora creo diversi piccoli metodi
+    // cosi da avere una logica pulita e leggibile da usare poi dentro createPrenotazione.
+
+    private void validaRichiesta(CrePrenotazioneRequest req){
+        if(req.getNumeroPartecipanti() == null || req.getNumeroPartecipanti() <= 0){
+            throw new IllegalArgumentException  ("numeri posti non disponibili");
+        }
+        if(req.getDisponibilitaItinerarioId() == null && req.getSessioneSingolaAttivitaId() == null ){
+            throw new IllegalArgumentException ("Devi selezionare un itinerario o una singola attività");
+        }
+        if(req.getDisponibilitaItinerarioId() != null && req.getSessioneSingolaAttivitaId() != null ){
+            throw new IllegalArgumentException ("Devi selezionare un itinerario o una singola attività");
+        }
     }
+    private DisponibilitaItinerario recuperaDisponibilita (Long id){
+        Optional<DisponibilitaItinerario> dispon = disponibilitaItinerarioRepository.findById(id);
+        if(!dispon.isPresent()){
+            throw new IllegalArgumentException("Itinerario non trovato");
+        }
+        return dispon.get();
+
+    }
+    private SessioneSingolaAttivita recuperaSingolaAttivita (Long id){
+        Optional<SessioneSingolaAttivita> sessioneSing = sessioneSingolaAttivitaRepository.findById(id);
+        if(!sessioneSing.isPresent()){
+            throw new IllegalArgumentException("Sessione non trovata");
+        }
+        return sessioneSing.get();
+    }
+
+    // piu avanti evito la duplicazioni dei due metodi scalaposti.
+    private void controllaEScalaPostiSessione(SessioneSingolaAttivita sessione, Integer numeroPartecipanti){
+        if (sessione.getPostiDisponibili()<numeroPartecipanti ){
+            throw new IllegalArgumentException("Numero posti non disponibili");
+        }
+        sessione.setPostiDisponibili(sessione.getPostiDisponibili() - numeroPartecipanti);
+
+    }
+    private void controllaEScalaPostiItinerario(DisponibilitaItinerario disp, Integer numeroPartecipanti){
+        if (disp.getPostiDisponibili()<numeroPartecipanti ){
+            throw new IllegalArgumentException("Numero posti non disponibili");
+        }
+        disp.setPostiDisponibili(disp.getPostiDisponibili() - numeroPartecipanti);
+    }
+
+    // Lo compatto appena capisco e cerco poi di evitare i duplicati come con scala posto. Sorry guys.
+    private BigDecimal calcolaPrezzoItinerario(DisponibilitaItinerario disp, Integer numeroPartecipanti) {
+        BigDecimal prezzoBase = disp.getItinerario().getPrezzoBase();
+        BigDecimal partecipanti = BigDecimal.valueOf(numeroPartecipanti); // converto Big in un inter int se non erro
+        // cosi posso moltiplicare (dannato Bigdecimal)
+        return prezzoBase.multiply(partecipanti);
+    }
+
+    private BigDecimal calcolaPrezzoSessioneSingola(SessioneSingolaAttivita sessione, Integer numeroPartecipanti) {
+        BigDecimal prezzoBase = sessione.getSingolaAttivita().getPrezzo();
+        BigDecimal partecipanti = BigDecimal.valueOf(numeroPartecipanti); // converto Big in un inter int se non erro
+        // cosi posso moltiplicare (dannato Bigdecimal)
+        return prezzoBase.multiply(partecipanti);
+    }
+
+
+
 
     @Transactional
     public Prenotazione createPrenotazione(CrePrenotazioneRequest req) {
-        //partiamo da un controllo base, ovvero utente o sceglie un viaggio o lun it, senno errore
-        if (req.getNumeroPartecipanti() == null || req.getNumeroPartecipanti() <= 0) {
-            throw new IllegalArgumentException("Numero partecipanti non valido");
-        }
 
-        boolean isItinerario = req.getDisponibilitaItinerarioId() != null;
-        boolean isSessione = req.getSessioneSingolaAttivitaId() != null;
 
-        if (isItinerario == isSessione) {
-            throw new IllegalArgumentException("Devi scegliare o un itinerario o una singola attività");
-        }
-        //momentaneo
+
         return null;
     }
 }
