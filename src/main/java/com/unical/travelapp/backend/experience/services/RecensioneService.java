d5af13567d3e1d4691e9dc5030fa87e0a6343455
Alessandro-CC05 <aleandroc05@gmail.com>
2026-05-18 17:39:38 +0200

    salvo

--- src/main/java/com/unical/travelapp/backend/experience/services/RecensioneService.java
+++ src/main/java/com/unical/travelapp/backend/experience/services/RecensioneService.java
@@ -4,7 +4,12 @@ import com.unical.travelapp.backend.booking.entity.Prenotazione;
 import com.unical.travelapp.backend.experience.models.DTO.RecensioneDTO;
 import com.unical.travelapp.backend.experience.models.Recensione;
 import com.unical.travelapp.backend.experience.repository.RecensioneRepository;
+import com.unical.travelapp.backend.identity.entity.Utente;
+import com.unical.travelapp.backend.identity.service.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,6 +19,7 @@ public class RecensioneService {
 
     @Autowired
     private RecensioneRepository repo;
+    private UtenteService utenteService;
 
     public RecensioneDTO getById(Long id){
         RecensioneDTO dto = null;
@@ -36,15 +42,35 @@ public class RecensioneService {
         return dto;
     }
 
-    public boolean addNewRecensione(RecensioneDTO dto){
-        Recensione recensione = new Recensione();
-        try{
-            recensione.setPrenotazione(dto.getPreno());
+    public boolean addNewRecensione(RecensioneDTO dto) {
+
+        try {
+            Authentication authentication =
+                    SecurityContextHolder.getContext().getAuthentication();
+
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+
+            Utente utente = utenteService.ottieniUtenteDaToken(jwt);
+
+            Prenotazione prenotazione = prenotazioneRepository
+                    .findById(dto.getPrenotazioneId())
+                    .orElseThrow();
+
+            if (!prenotazione.getUtente().getId().equals(utente.getId())) {
+                throw new RuntimeException("Non autorizzato");
+            }
+
+            Recensione recensione = new Recensione();
+            recensione.setUtente(utente);
+            recensione.setPrenotazione(prenotazione);
             recensione.setCommento(dto.getComm());
             recensione.setVoto(dto.getVotazione());
+
             repo.save(recensione);
+
             return true;
-        }catch(Exception e){
+
+        } catch (Exception e) {
             System.out.println(e);
             return false;
         }
