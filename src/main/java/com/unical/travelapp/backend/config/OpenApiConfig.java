aeb0b4ce6cf63b1633dbb0d45555c7b8e4a0f88c
giuliacardamone2 <crdgli04h51m208g@studenti.unical.it>
2026-05-18 09:48:21 +0200

    feat: aggiunto OpenApiConfig

---
+++ src/main/java/com/unical/travelapp/backend/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.unical.travelapp.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "TravelApp API",
+                version = "1.0",
+                description = "Documentazione delle API del backend TravelApp"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "Inserisci il token JWT ottenuto da Keycloak"
+)
+public class OpenApiConfig {
+}
