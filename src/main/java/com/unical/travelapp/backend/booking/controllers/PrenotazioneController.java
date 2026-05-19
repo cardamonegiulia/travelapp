a505e905db45784f9bbf71572bc8b1f0db65db3a
Alex107219 <alexgenca10@gmail.com>
2026-05-19 19:21:14 +0200

    controller prenotazione, base, finito. Ora faccio dei test con postman e poi passo alla creazione del DTO response

--- src/main/java/com/unical/travelapp/backend/booking/controllers/PrenotazioneController.java
+++ src/main/java/com/unical/travelapp/backend/booking/controllers/PrenotazioneController.java
@@ -4,12 +4,12 @@ import com.unical.travelapp.backend.booking.dto.CreaPrenotazioneRequest;
 import com.unical.travelapp.backend.booking.entity.Prenotazione;
 import com.unical.travelapp.backend.booking.service.PrenotazioneService;
 import lombok.AllArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/prenotazioni")
@@ -22,4 +22,36 @@ public class PrenotazioneController {
         Prenotazione prenotazione = prenotazioneService.createPrenotazione(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(prenotazione);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Prenotazione> getPrenotazione(@PathVariable Long id) {
+        Prenotazione prenotazione = prenotazioneService.getPrenotazioneById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(prenotazione);
+    }
+
+    @GetMapping("/utente/{utenteId}")
+    public ResponseEntity<List<Prenotazione>> getPrenotazioniByIdUtente(@PathVariable Long utenteId) {
+        List<Prenotazione> prenotazioni = prenotazioneService.getPrenotazioniByUtente(utenteId);
+        return ResponseEntity.status(HttpStatus.OK).body(prenotazioni);
+    }
+
+    @PostMapping("/{id}/paga")
+    public ResponseEntity<Prenotazione> pagaPrenotazione(@PathVariable Long id) {
+        Prenotazione prenotazione = prenotazioneService.pagaPrenotazione(id);
+        return ResponseEntity.status(HttpStatus.OK).body(prenotazione);
+    }
+
+    @PostMapping("/{id}/annulla")
+    public ResponseEntity<Prenotazione> annullaPrenotazione(@PathVariable Long id) {
+        Prenotazione prenotazione = prenotazioneService.annullaPrenotazione(id);
+        return ResponseEntity.status(HttpStatus.OK).body(prenotazione);
+    }
+
+
+
+
+
+
+
+
 }
