91b127340426501e87ba713386dbc38de40b657d
Alessandro-CC05 <aleandroc05@gmail.com>
2026-05-24 19:11:01 +0200

    Salvataggio temporaneo: setup di VS Code fatto dall'LLM

--- src/main/java/com/unical/travelapp/backend/experience/models/DTO/RecensioneDTO.java
+++ src/main/java/com/unical/travelapp/backend/experience/models/DTO/RecensioneDTO.java
@@ -2,10 +2,8 @@ package com.unical.travelapp.backend.experience.models.DTO;
 
 import com.unical.travelapp.backend.booking.entity.Prenotazione;
 import com.unical.travelapp.backend.identity.entity.Utente;
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 public class RecensioneDTO {
