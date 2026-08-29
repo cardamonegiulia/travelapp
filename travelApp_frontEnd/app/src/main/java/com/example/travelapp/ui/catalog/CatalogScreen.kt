e43926a6fe0a4e18e66722a817e658c428f1d641
Alex107219 <alexgenca10@gmail.com>
2026-08-29 16:46:35 +0200

    fix generali

--- travelApp_frontEnd/app/src/main/java/com/example/travelapp/ui/catalog/CatalogScreen.kt
+++ travelApp_frontEnd/app/src/main/java/com/example/travelapp/ui/catalog/CatalogScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.travelapp.domain.model.Itinerario
 import com.example.travelapp.domain.model.SingolaAttivita
+import com.example.travelapp.ui.theme.BackgroundLavender
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,11 +26,12 @@ fun CatalogScreen(
     val state by viewModel.uiState.collectAsState()
 
     Scaffold(
+        containerColor = BackgroundLavender,
         topBar = {
             TopAppBar(
                 title = { Text("Catalogo Viaggi") },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.background
+                    containerColor = BackgroundLavender
                 )
             )
         }
